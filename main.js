5ee553991ec2a265363bb0ae7a474f4443a069d6
David Baeza <davidbc_98@hotmail.com>
2024-03-08 19:42:21 +0100

    js

---
+++ main.js
@@ -0,0 +1,20 @@
+// Ejercicio 1
+// let nombre = "David";
+// alert(nombre);
+
+
+
+//Ejercicio 2 
+//let nombre = prompt("Introduce tu nombre" );
+//alert(nombre);
+
+// Ejercicio 1.1
+// let nombre;
+// nombre = "David";
+// console.log(typeof nombre);
+
+// if(true) {
+   //  let nombre = "Sara";
+    // console.log(nombre);
+// }
+// console.log(nombre);
