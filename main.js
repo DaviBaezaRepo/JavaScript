0d7720a046afbd0cb00dd9aa9ec7796646c4d5b4
David Baeza <davidbc_98@hotmail.com>
2024-03-12 19:14:37 +0100

    js

--- main.js
+++ main.js
@@ -18,3 +18,36 @@
     // console.log(nombre);
 // }
 // console.log(nombre);
+
+//Ejercicio 2 return
+
+// let nombreyapellido = (name,surname) => {
+//     name = ("David");
+//     surname = ("Baeza")
+//     return ("Mi nombre es: " + name + " " + surname);
+// } 
+// console.log(nombreyapellido());
+
+//Ejercicio Boolean 
+// function nombre () {
+//     nombre = false;
+//         console.log(!nombre);
+// }
+// nombre ();
+
+//Ejercicio ForEach funcion dentro de una funcion 
+
+// function parametros(...numbers){
+//     numbers.forEach(number => console.log(numbers));
+// }
+// parametros(1,5,3,4,5);
+
+// let cara = 1;
+// cruz = 0;
+// function random () {
+//     return math.round(math.random(cara,cruz))
+// }
+
+// console.log(random);
+
+
