54c5b567fbe73ed7a961697b457e551ce505e917
David Baeza <davidbc_98@hotmail.com>
2024-03-13 18:02:17 +0100

    js

--- main.js
+++ main.js
@@ -42,16 +42,164 @@
 // }
 // parametros(1,5,3,4,5);
 
+// Ejercicio Cara o cruz
+// function random () {
+//     let moneda = Math.round(Math.random());
+//     if(moneda){
+//         console.log("Te ha salido cara");
+//     }else{
+//         console.log("Te ha salido cruz");
+//     }
+// }
 
-function random () {
-    let moneda = Math.round(Math.random());
-    if(moneda){
-        console.log("Te ha salido cara");
-    }else{
-        console.log("Te ha salido cruz");
-    }
-}
+// random();
 
-random();
+// Ejercicio funcion suma con return 
+
+// function suma (param1,param2,param3){
+//     return (param1 + param2 + param3);
+// }
+// console.log(suma(6, 14, 3));
+
+// Ejercicio nombre y apellidos
+// function nomcompleto(nombre,apellido1,apellido2){
+//     nombre = "David";
+//     apellido1 = "Baeza";
+//     apellido2 = "Castillo";
+//     console.log("Mi nombre es: ", nombre + apellido1 + apellido2);
+// }
+// nomcompleto();
 
 
+// Ejercicio cual es el numero mayor
+// function numeromayor (num1,num2){
+//     if(num1 > num2){
+//         console.log(num1 + " este numero es mayor")
+//     }else{
+//         console.log(num2 + " este numero es mayor")
+//     }
+// }
+// numeromayor(3,2);
+
+// Ejercicio meses con switch
+
+// let mes = (7);
+// switch (mes){
+//     case 1:
+//         console.log("Este mes es enero");
+//         break;
+//     case 2:
+//         console.log("Este mes es febrero");
+//         break;
+//     case 3:
+//         console.log("Este mes es marzo");
+//         break;
+//     case 4:
+//         console.log("Este mes es abril");
+//         break;
+//     case 5:
+//         console.log("Este mes es mayo");
+//         break;
+//     case 6:
+//         console.log("Este mes es junio");
+//         break;
+//     case 7:
+//         console.log("Este mes es julio");
+//         break;
+//     case 8:
+//         console.log("Este mes es agosto");
+//         break;
+//     case 9:
+//         console.log("Este mes es septiembre");
+//         break;
+//     case 10:
+//         console.log("Este mes es octubre");
+//         break;
+//     case 11:
+//         console.log("Este mes es noviembre");
+//         break;
+//     case 12:
+//         console.log("Este mes es diciembre");
+//         break;
+// }
+
+//Ejercicio switch del uno al cinco
+
+// let numero = ("6")
+
+// switch (numero){
+//     case "1":
+//         console.log(Number(numero));
+//         break;
+//     case "2":
+//         console.log(parseInt(numero));
+//         break;
+//         case "3":
+//             console.log(parseInt(numero));
+//             break;
+//             case "4":
+//                 console.log(parseInt(numero));
+//                 break;
+//                 case "5":
+//                     console.log(parseInt(numero));
+//                     break;
+//                     default:
+//                         console.log("Numero desconocido")
+//                         break;
+// }
+
+
+//Ejercicio Math random
+
+// function random(){
+//     let numero = Math.round(Math.random());
+//     console.log(numero)
+// }
+// random();
+
+//Ejercicio redondear numero pie
+
+// function redondeo(){
+//     console.log(Math.round(Math.PI));
+// }
+
+// redondeo();
+
+
+//Ejercicio substituir letras a y o
+
+// function cambio(){
+//     let letra = "a";
+//     console.log(letra.replace("a", "o"))
+// }
+// cambio();
+
+//Ejercicio empieza por 
+// function comprobacion(){
+//     let param1 = "academia";
+//     let param2 = "escuela";
+//     console.log(param1.startsWith("aca"));
+//     console.log(param2.startsWith("aca"));
+// }
+
+// comprobacion();
+
+//Ejercicio Hola 3 veces
+
+// let hola = "hola ";
+// function repetir(){
+//     console.log(hola.repeat(3));
+// }
+// repetir();
+
+//Ejercicio i ❤ code 10 veces con for y while
+
+// for (let love = 0; love < 10; love++){
+//     console.log("I ❤ code")
+// }
+
+// let love = 0;
+// while (love < 10) {
+//     console.log("I ❤ code");
+//     love++;
+// }
